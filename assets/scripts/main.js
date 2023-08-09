8be9f0c765246c5dc816c507c74146f6fcb39e7d
glinskyoff <omsk.den@bk.ru>
2023-08-09 11:56:31 +0600

    adaptive v1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,4 +12,21 @@ $(document).ready(function () {
       );
     }
   });
+
+  $(".more").click(function (e) {
+    $(".menuBlock").toggleClass("active");
+    $("body").toggleClass("overflow");
+  });
+  $(".menuBlockLi").click(function (e) {
+    $(".menuBlock").removeClass("active");
+    $("body").removeClass("overflow");
+  });
 });
+
+// $(".more").toggle("slow", function () {
+//   $(".menuBlock").css("display", "block");
+// });
+
+// $(".menu__item").on("click", function () {
+//   $(".menu__box").css("left", "-100%");
+// });
